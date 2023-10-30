72372c74b59e17afddd20ff111e2354462cafd53
miyazawa <13tmtmtommy@gmail.com>
2023-10-30 14:13:36 +0900

    API通信実装

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:qiita_search/models/article.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -16,5 +21,32 @@ class _SearchScreenState extends State<SearchScreen> {
       ),
       body: Container(),
     );
+  }
+
+  Future<List<Article>> searchQiita(String keyword) async {
+    // 1.http通信に必要なデータを準備する
+    // - URL,パラメータ設定
+    final uri = Uri.https('qiita.com', '/api/v2/items', {
+      'query': 'title:$keyword',
+      'per_page': 10,
+    });
+    // - アクセストークンを取得
+    final String token = dotenv.env['QIITA_ACCESS_TOKEN'] ?? '';
+
+    // 2.Qiita APIにリクエストを送る
+    // - アクセストークンを含めてリクエストを送信
+    final http.Response res = await http.get(uri, headers: {
+      'Authorization': 'Bearer $token',
+    });
+    // 3.戻り値をArticleクラスの配列に変換
+    // 4.変換したArticleクラスの配列を返す
+
+    if (res.statusCode == 200) {
+      // モデルクラスへ変換
+      final List<dynamic> body = jsonDecode(res.body);
+      return body.map((dynamic json) => Article.fromJson(json)).toList();
+    } else {
+      return [];
+    }
   }
 }
